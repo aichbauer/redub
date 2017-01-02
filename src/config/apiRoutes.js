9533dfe7b0826aa2cdc064851742e6af3ea0e720
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-01-02 19:09:43 +0100

    add: config, configuring th e api routes

---
+++ src/config/apiRoutes.js
@@ -0,0 +1,5 @@
+export const BASE_API_URL     = "http://localhost:1337";
+
+export const LOGIN_URL        = "/api/login";
+export const REGISTER_URL     = "/api/register";
+export const VALID_TOKEN_URL  = "/api/isValid";
