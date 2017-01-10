ca2e3a718afd6a4847d4960062d79fb3c7c7bf25
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-01-10 14:39:42 +0100

    fix: webpack.config.js -> typo loader to loaders

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       {
 
         test: /\.scss$/,
-        loader: ['style-loader', 'css-loader', 'sass-loader']
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
 
       }
 
