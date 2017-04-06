86b6dc0742b827ea72b29a1e1b4da25e6d1340ae
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-04-07 01:35:40 +0200

    :sparkles: Feat: delete scss, add styled-components

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,30 @@
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-
   devServer: {
-
     inline: true,
     contentBase: './dist',
-    port: 3000
-
+    port: 3000,
   },
   entry: './src/js/app.js',
   output: {
-
     path: './dist',
-    filename: 'js/app.bundle.js'
-
+    filename: 'js/app.bundle.js',
   },
   module: {
-
     loaders: [
-
       {
-
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: ['babel']
-
+        loaders: ['babel'],
       },
-      {
-
-        test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
-
-      }
-
-    ]
-
+    ],
   },
   plugins: [
-
     new CopyWebpackPlugin([
-
       {
-
-        from: 'src/index.html'
-
-      }
-
-    ])
-
-  ]
-
+        from: 'src/index.html',
+      },
+    ]),
+  ],
 };
