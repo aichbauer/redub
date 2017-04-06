018b33432515d58be91a1c3ca98e01818a41db7f
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-04-07 01:30:06 +0200

    :sparkles: Feat: add redux-devtools-extension

--- src/js/app.js
+++ src/js/app.js
@@ -1,73 +1,42 @@
-'use strict';
-
-
 import 'babel-polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { 
-  
-  createStore, 
-  applyMiddleware 
-
-} from 'redux';
-import { 
-
-  Router, 
-  Route, 
-  IndexRoute, 
-  hashHistory 
-
+import {
+  hashHistory,
+  IndexRoute,
+  Route,
+  Router,
 } from 'react-router';
-import { 
-  
-  syncHistoryWithStore, 
-  routerMiddleware 
-
+import {
+  routerMiddleware,
+  syncHistoryWithStore,
 } from 'react-router-redux';
+import {
+  applyMiddleware,
+  createStore,
+} from 'redux';
 import ReduxThunk from 'redux-thunk';
-import DevTools from './components/devTools/devTools';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
 
 import allReducers from './reducers/index';
 
 import App from './containers/wrapper/app';
 
-import index from './views/index/index';
+import index from './views/index';
 
 
 const routingMiddleware = routerMiddleware(hashHistory);
-//TODO: create a production file without dev tools and a development file with dev tools
-const enhancer = compose(
 
+const store = createStore(allReducers, composeWithDevTools(
   applyMiddleware(ReduxThunk, routingMiddleware),
-  DevTools.instrument()
+));
 
-);
-
-function configureStore(initialState) {
-  
-  const store = createStore(allReducers, initialState, enhancer);
-
-  if (module.hot) {
-
-    module.hot.accept('./reducers/index', () =>
-
-      store.replaceReducer(require('./reducers/index'))
-
-    );
-
-  }
-
-  return store;
-
-}
-
-const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
 
-
 ReactDOM.render(
-
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App}>
@@ -75,8 +44,6 @@ ReactDOM.render(
       </Route>
     </Router>
   </Provider>,
-
-  document.getElementById('root')
-
+  document.getElementById('root'),
 );
 
